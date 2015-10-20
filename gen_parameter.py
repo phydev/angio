06b91207f5a1db279d6d4420659cefbe4095dbb1
phydev <maninho.g3@gmail.com>
2015-10-20 18:15:30 +0100

    chi parameter

--- gen_parameter.py
+++ gen_parameter.py
@@ -2,8 +2,8 @@
 # for a range of parameters
 
 # which parameter would you like to vary?
-alpha_p_of = True
-chi_of = False
+alpha_p_of = False
+chi_of = True
 
 alpha_p_min, alpha_p_max, delta_alpha_p = 0.1, 3.0, 0.2
 chi_min, chi_max, delta_chi = 200, 2000, 100
@@ -65,4 +65,49 @@ if(chi_of):
     chi = chi_min
 
     while chi <= chi_max:
-        chi += delta_chi
+
+        filename = 'inp'+counters
+        dir_name = 'dir'+counters
+
+        file = open(filename, 'w')
+
+        file.write('4.0, cell_radius \n')
+        file.write('100.0, diffusion_const \n')
+        file.write('1.00, interface_width \n')
+        file.write('0.30, vegf_p \n')
+        file.write('0.10, vegf_c \n')
+        file.write('20.0, diff_oxy_length \n')
+        file.write('6.25, vegf_rate \n')
+        file.write('1.00, vegf_source_conc \n')
+        file.write('1.0, prolif_rate \n')
+        file.write('5.00, vessel_radius \n')
+        file.write('150000, total_time_step \n')
+        file.write('0.0005, dt \n')
+        file.write(str(chi)+', chi_chemiotactic_resp \n')
+        file.write('80, 80, 50, 1, 1, 1, Lx_Ly_Lz_dx_dy_dz \n')
+        file.write(dir_name+', dir_name \n')
+        file.write('100000, random_seed \n')
+        file.write('20, number_of_boundary_points \n')
+        file.write('500, source_max \n')
+        file.write('0.01, vegf_grad_min \n')
+        file.write('0.03, vegf_max \n')
+        file.write('2.00, depletion_weight \n')
+        file.write('2000, output_period \n')
+        file.write('40000, extra_steps \n')
+        file.write('500, max_number_of_tip_cells \n')
+        file.write('T, thinning_FT \n')
+        file.write('F, periodic_FT \n')
+        
+        chi += delta_chi         
+        
+        counteri += 1
+
+        if(counteri<10):
+            counters = '0'+str(counteri)
+        else:
+            counters = str(counteri)
+
+        file.close()
+
+
+        
