52731f9fb29bf6ca6d0d08f364e6a4d8944ec8d0
phydev <maninho.g3@gmail.com>
2016-06-28 10:47:11 +0100

    v5.5.u.mod

---
+++ compile.sh
@@ -0,0 +1,4 @@
+mkdir mod
+dir='./src'
+ifort -r8 -mkl -O3 -module mod $dir/global.F90 $dir/dderivatives.F90 $dir/misc.F90 $dir/init.F90 $dir/source.F90 $dir/etcdyn.F90 $dir/thinning.F90 $dir/blood_flow.F90 $dir/run_angio.F90 $dir/main.F90 -o angio_flow 
+
