7a66b2996a7ab0013d8a43db6c1803a68bc81d17
João Braz Simões <joaosbraz@gmail.com>
2023-10-24 09:29:23 +0100

    Fix dockerization for Apple Silicon

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 gfortran -o angio-flow -fdefault-real-8 src/global.F90 src/dderivatives.F90 src/misc.F90 src/init.F90 src/source.F90 src/etcdyn.F90 src/thinning.F90 src/blood_flow.F90 src/congraph.F90 src/run_angio.F90 src/main.F90 /usr/lib/x86_64-linux-gnu/lapack/liblapack.so.3.9.0
 
-./angio-flow 001
+./angio-flow "$1"
